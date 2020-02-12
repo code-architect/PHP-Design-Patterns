b097bdba05815ea582fa5de4e0921af5dcf43cb7
code-architect <codearchitectdeveloper@gmail.com>
2020-02-13 04:30:32 +0530

    using dd and dump functions from laravel

--- active_record_pattern/index.php
+++ active_record_pattern/index.php
@@ -1,12 +1,12 @@
 <?php
 require '../vendor/autoload.php';
 
-$users = User::all();
-
-foreach ($users as $user)
-{
-    echo "ID: ".$user->id."<br>";
-    echo "Name: ".$user->name."<br>";
-    echo "Email: ".$user->email;
-    echo "<hr>";
-}
+//$users = User::all();
+//dd($users);
+//foreach ($users as $user)
+//{
+//    echo "ID: ".$user->id."<br>";
+//    echo "Name: ".$user->name."<br>";
+//    echo "Email: ".$user->email;
+//    echo "<hr>";
+//}
